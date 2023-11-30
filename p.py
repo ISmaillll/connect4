8c7a654aa5d4a6e6631fd4f0ca236e2b3e360d11
I_Smail <106024751+ISmaillll@users.noreply.github.com>
2023-11-30 21:56:40 +0100

    Add files via upload

--- p.py
+++ p.py
@@ -12,27 +12,39 @@ MIN = -1
 
 class ConnectFourBoard :
 
-    def __init__(self): 
+    def __init__(self,depth=0,piece=MAX): 
         self.board = [['_' for _ in range(7)] for _ in range(6)]
-        self.h = 0
+        self.piece = piece
+        self.depth = depth
+        self.Action = (0,0)
+        self.NextAction = (0,0)
+        self.value = 0
+        self.alpha = 0
+        self.beta = 0
         
-    def utilFn(self,piece):
+    def Win_Value(self,piece):
         if piece == MAX: return inf
         else: return -inf
 
     def heuristicEval(self,piece):
-        pass
+        if piece == MAX: return 10
+        else: return -10
 
     def getPossibleMoves(self,piece):
         succs = list()
         for j in range(7):
-            for i in range(6):
+            find = False
+            for i in range(5, 0, -1):
                 if self.board[i][j]=='_':
+                    find=True
                     successor = deepcopy(self)
+                    successor.depth = self.depth+1
+                    successor.piece = self.piece*-1
+                    successor.Action = (i,j)
                     successor.makeMove(i,j,piece)
                     break
-            if i!=0:
-                succs.append((i,j),successor)
+            if find:
+                succs.append(successor)
         return succs
     
     def drawBoard(self):
@@ -83,11 +95,14 @@ class ConnectFourBoard :
         else:
             print("Yellow win")
 
-    def gameOver(self):
+    def Tie(self):
         for j in range(7):
             if self.board[0][j] == '_':
                 return False
         return True
+
+    def gameOver(self,piece):
+        return self.Tie() or self.win(piece)
 
     def moveInCol(self,col):
         find = False
@@ -105,24 +120,64 @@ class Play:
 
     def humanTurn() :
         pass
-    
-    def computerTurn() :
-        pass
+    @staticmethod
+    def computerTurn(state) :
+        NextAction , NextValue = Play.minimaxAlphaBetaPruning(state,2,-inf,+inf,MAX)
+        return NextAction
 
     @staticmethod
-    def playrandom() :
-        return randint(0, 6)
+    def playrandom(state) :
+        play_row = -1
+        while play_row == -1:
+            play_col=randint(0, 6)
+            play_row=state.moveInCol(play_col)
+        
+        return play_col , play_row
     
     @staticmethod
     def play(state,Turn):
-        play_col=Play.playrandom()
-        play_row=state.moveInCol(play_col)
+        if Turn == MAX:
+            print("AI")
+            play_col , play_row = Play.computerTurn(state)
+        else :
+            print("Random")
+            play_col , play_row = Play.playrandom(state)
         print(play_row,play_col)
-        if play_row >=0:
-            state.makeMove(play_row,play_col,Turn)
-        return state
-    def MinimaxAlphaBetaPruning():
-        pass
+
+        return play_col , play_row
+    @staticmethod
+    def minimaxAlphaBetaPruning(state, depthlim, alpha=-inf, beta=+inf, piece=MAX):
+        if state.depth < depthlim or not state.gameOver(state.piece):
+            if piece==MAX:
+                state.value = -inf
+                state.alpha = -inf
+                state.beta = +inf
+                for successor in state.getPossibleMoves(piece):
+                    NextAction , NextValue = Play.minimaxAlphaBetaPruning(successor,depthlim,state.alpha,state.beta,MIN)
+                    state.value = max(state.value,NextValue)
+                    state.alpha = max(state.value,alpha)
+                    if state.value == NextValue:state.NextAction = NextAction
+                    if state.value >=beta:
+                        return state.Action,state.value 
+            else:
+                state.value = inf
+                state.alpha = -inf
+                state.beta = inf
+                for successor in state.getPossibleMoves(piece):
+                    NextAction , NextValue = Play.minimaxAlphaBetaPruning(successor,depthlim,state.alpha,state.beta,MAX)
+                    state.value = min(state.value,NextValue)
+                    state.beta = min(state.value,beta)
+                    if state.value == NextValue:state.NextAction = NextAction
+                    if state.value <=alpha:
+                        return state.Action,state.value
+        if state.gameOver(state.piece):
+            if state.Tie():
+                state.value = 0
+            else:
+                state.value = state.Win_Value(state.piece)
+        else :
+            state.value = state.heuristicEval(state.piece)
+        return state.Action,state.value    
 
 def main():
     playing = True
@@ -131,15 +186,20 @@ def main():
     state.drawBoard()
 
     while(playing):
-        state = Play.play(state,Turn)
+        #
+        play_col , play_row = Play.play(state,Turn)
+        state.makeMove(play_row,play_col,Turn)
         state.drawBoard()
         time.sleep(0.4)
-        if state.win(Turn):
+        # end game 
+        if state.gameOver(Turn):
+            if state.Tie():
+                print("tie")
+            else :
+                ConnectFourBoard.wtireWiner(Turn)
             playing = False
-            ConnectFourBoard.wtireWiner(Turn)
-        elif state.gameOver():
-            playing = False
-            print("taie")
+            
+        # change player
         Turn*=-1
 
 if __name__ == "__main__":
