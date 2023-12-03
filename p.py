befd93f5f63d664c3d5f4fed7ae9a9782b7bdfdf
I_Smail <106024751+ISmaillll@users.noreply.github.com>
2023-12-03 17:29:24 +0100

    Add files via upload

--- p.py
+++ p.py
@@ -14,8 +14,8 @@ class ConnectFourBoard :
 
     def __init__(self,depth=0,piece=MAX): 
         self.board = [['_' for _ in range(7)] for _ in range(6)]
-        self.piece = piece
-        self.depth = depth
+        self.piece = piece # 1 or -1
+        self.depth = depth # 1 to depthlim
         self.Action = (0,0)
         self.NextAction = (0,0)
         self.value = 0
@@ -23,12 +23,56 @@ class ConnectFourBoard :
         self.beta = 0
         
     def Win_Value(self,piece):
-        if piece == MAX: return inf
-        else: return -inf
+        if piece == MAX: return 100
+        else: return -100
 
-    def heuristicEval(self,piece):
-        if piece == MAX: return 10
-        else: return -10
+    def heuristicEval1(self,piece):
+        row , col = self.Action
+        if col > 3 : 
+            col-=6
+            col*=-1
+        H = 1+col
+        if piece == MAX: return -H
+        else: return H
+
+    def heuristicEval2(self,piece):
+        score = 0
+        for row in range(6):
+            for col in range(4):
+                score += self.score_window([self.board[row][col], self.board[row][col + 1],
+                                            self.board[row][col + 2], self.board[row][col + 3]])
+        for col in range(7):
+            for row in range(3):
+                score += self.score_window([self.board[row][col], self.board[row + 1][col],
+                                            self.board[row + 2][col], self.board[row + 3][col]])
+        for row in range(3, 6):
+            for col in range(4):
+                score += self.score_window([self.board[row - i][col + i] for i in range(4)])
+        for row in range(3, 6):
+            for col in range(3, 7):
+                score += self.score_window([self.board[row - i][col - i] for i in range(4)])
+        if piece == MAX: return -score
+        else: return score
+    def score_window(self, window):
+        if window.count(MAX) == 4:
+            return 100
+        elif window.count(MAX) == 3 and window.count('_') == 1:
+            return 5
+        elif window.count(MAX) == 2 and window.count('_') == 2:
+            return 2
+        elif window.count(MIN) == 2 and window.count('_') == 2:
+            return -2
+        elif window.count(MIN) == 3 and window.count('_') == 1:
+            return -5
+        elif window.count(MIN) == 4:
+            return -100
+        else:
+            return 0
+        
+    def heuristicChois(self,piece,HChois):
+        match HChois:
+            case 1:return self.heuristicEval1(piece)
+            case 2:return self.heuristicEval2(piece)
 
     def getPossibleMoves(self,piece):
         succs = list()
@@ -117,14 +161,19 @@ class ConnectFourBoard :
         else : return -2  
           
 class Play:
-
-    def humanTurn() :
+    DepthLim = 1
+    def humanTurn() : 
         pass
     @staticmethod
-    def computerTurn(state) :
-        NextAction , NextValue = Play.minimaxAlphaBetaPruning(state,3,-inf,+inf,MAX)
+    def computerTurn1(state) :
+        NextAction , _ = Play.minimaxAlphaBetaPruning(state,Play.DepthLim,-inf,+inf,MAX,1)
         return NextAction
-
+    
+    @staticmethod
+    def computerTurn2(state) :
+        NextAction , _ = Play.minimaxAlphaBetaPruning(state,Play.DepthLim,-inf,+inf,MAX,2)
+        return NextAction
+    
     @staticmethod
     def playrandom(state) :
         play_row = -1
@@ -132,31 +181,32 @@ class Play:
             play_col=randint(0, 6)
             play_row=state.moveInCol(play_col)
         
-        return play_col , play_row
+        return play_row , play_col  
     
     @staticmethod
     def play(state,Turn):
         if Turn == MAX:
             print("AI")
-            play_row,play_col  = Play.computerTurn(state)
+            play_row,play_col  = Play.computerTurn1(state)
         else :
             print("Random")
-            play_col , play_row = Play.playrandom(state)
+            play_row,play_col  = Play.computerTurn2(state)
         print(play_row,play_col)
 
         return play_col , play_row
     @staticmethod
-    def minimaxAlphaBetaPruning(state, depthlim, alpha=-inf, beta=+inf, piece=MAX):
+    def minimaxAlphaBetaPruning(state, depthlim, alpha=-inf, beta=+inf, piece=MAX,HChois=1):
         if state.depth < depthlim :
             if piece==MAX:
                 state.value = -inf
                 state.alpha = -inf
                 state.beta = +inf
                 for successor in state.getPossibleMoves(piece):
-                    NextAction , NextValue = Play.minimaxAlphaBetaPruning(successor,depthlim,state.alpha,state.beta,MIN)
-                    state.value = max(state.value,NextValue)
+                    NextAction , NextValue = Play.minimaxAlphaBetaPruning(successor,depthlim,state.alpha,state.beta,MIN,HChois)
+                    if state.value < NextValue:
+                        state.value = NextValue
+                        state.NextAction = NextAction
                     state.alpha = max(state.value,alpha)
-                    if state.value == NextValue:state.NextAction = NextAction
                     if state.value >=beta:
                         if state.depth == 0:
                             return state.NextAction , state.value
@@ -167,10 +217,11 @@ class Play:
                 state.alpha = -inf
                 state.beta = inf
                 for successor in state.getPossibleMoves(piece):
-                    NextAction , NextValue = Play.minimaxAlphaBetaPruning(successor,depthlim,state.alpha,state.beta,MAX)
-                    state.value = min(state.value,NextValue)
+                    NextAction , NextValue = Play.minimaxAlphaBetaPruning(successor,depthlim,state.alpha,state.beta,MAX,HChois)
+                    if state.value > NextValue:
+                        state.value = NextValue
+                        state.NextAction = NextAction
                     state.beta = min(state.value,beta)
-                    if state.value == NextValue:state.NextAction = NextAction
                     if state.value <=alpha:
                         if state.depth == 0:
                             return state.NextAction , state.value
@@ -182,7 +233,8 @@ class Play:
             else:
                 state.value = state.Win_Value(state.piece)
         else :
-            state.value = state.heuristicEval(state.piece)
+            state.value = state.heuristicChois(state.piece,HChois)
+        print(state.Action,state.value)
         if state.depth == 0:
             return state.NextAction , state.value
         else:
